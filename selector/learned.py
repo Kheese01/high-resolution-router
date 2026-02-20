744261b557bc637f9660fec3a845d4cd5e1033ce
Kheese <sudoryo11111@gmail.com>
2026-02-21 02:19:37 +0900

    fix: the function became callable

--- selector/learned.py
+++ selector/learned.py
@@ -3,10 +3,15 @@ from .features import extract_features
 from .base import BaseSelector
 
 class LearnedSelector(BaseSelector):
-    def __init__(self, model_path):
-        self.clf = joblib.load(model_path)
+    def __init__(self, model_path, device="cpu"):
+        self.model_path = model_path
+        self.device = device
+        self.clf = joblib.load(model_path)  
 
     def select(self, img):
         x = extract_features(img)[None, :]
         pred = self.clf.predict(x)[0]
         return pred
+    
+    def __call__(self, img):
+        return self.select(img)
