7caf1508bed11374426146e0fc84f94b810a1bec
Kheese01 <sudoryo11111@gmail.com>
2026-03-09 22:47:47 +0900

    feat: improve dataset builder and add SSIM support (#12)

--- datasets/metrics.py
+++ datasets/metrics.py
@@ -1,7 +1,23 @@
 import numpy as np
+from skimage.metrics import structural_similarity
 
 def psnr(img1, img2):
+
     mse = np.mean((img1.astype(float) - img2.astype(float)) ** 2)
+
     if mse == 0:
         return 100
+
     return 20 * np.log10(255.0 / np.sqrt(mse))
+
+
+def ssim(img1, img2):
+
+    score, _ = structural_similarity(
+        img1,
+        img2,
+        channel_axis=2,
+        full=True
+    )
+
+    return score
