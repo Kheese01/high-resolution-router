2857bd4c4a1394b0230743fb1e16bd47b406ca1c
Kheese <sudoryo11111@gmail.com>
2026-03-09 22:43:23 +0900

    feat: improve dataset builder and add SSIM support

--- datasets/builder.py
+++ datasets/builder.py
@@ -1,11 +1,14 @@
 from selector.features import extract_features
-from datasets.metrics import psnr
+from datasets.metrics import psnr, ssim
 
 def build_sample(lr, hr, enhancers):
     scores = {}
     for name, enh in enhancers.items():
         sr = enh.enhance(lr)
-        scores[name] = psnr(sr, hr)
+        scores[name] = {
+            "psnr": psnr(sr, hr),
+            "ssim": ssim(sr, hr)
+        }
 
-    label = max(scores, key=scores.get)
-    return extract_features(lr), label
+    best_model = max(scores, key=lambda k: scores[k]["psnr"])
+    return extract_features(lr), best_model, scores
